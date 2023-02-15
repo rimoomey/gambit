9ebd464af908db9184b3d7bbd8362bf980ee2f8a
Rimon Moomey <rimon.moomey@gmail.com>
2023-02-15 12:55:29 -0600

    url change

--- client/src/components/PlayPage.js
+++ client/src/components/PlayPage.js
@@ -27,7 +27,7 @@ export default function PlayPage() {
     if (user) {
       toast.dismiss("sign-in-toast");
       if (!cable) {
-        setCable(createConsumer(`ws://${backendURL}`));
+        setCable(createConsumer(`ws://${backendURL}cable`));
       }
     }
   }, [user]);
